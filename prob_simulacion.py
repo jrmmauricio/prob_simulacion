5ee2425a11ca25a0db312bc5d1ab34ecfbf45f88
jrmmauricio <56565940+jrmmauricio@users.noreply.github.com>
2021-12-13 16:00:11 -0400

    Actualizacion estructura de pregunta

--- prob_simulacion.py
+++ prob_simulacion.py
@@ -1,11 +1,16 @@
+def getProbability():
+     return 1/30
+    
 
-def getProbability(resp):
-    if (resp == 'y'):
-        return 1/30
+asking = False
+while (asking == False):
+    resp = input ('Desea conocer la probabilidad de escoger un estudiante de simulacion?(Y/N): ')
+    if (resp == 'y' or resp == 'Y'):
+        asking = True
+        print('Existe una probabilidad de: ', getProbability())
     else:
-        return 'adios.'
-
-resp = input ('Desea conocer la probabilidad de escoger un estudiante de simulacion?(Y/N).')
-print  (getProbability(resp))
-
+        if (resp == 'n' or resp == 'N'):
+            print('adios.')
+            asking = True
+    
 
