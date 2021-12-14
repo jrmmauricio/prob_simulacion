11a84fb02ac3e04a5ca197d44a5c5cf5985a54d0
jrmmauricio <56565940+jrmmauricio@users.noreply.github.com>
2021-12-14 10:33:45 -0400

    Simulacion acorde

--- prob_simulacion.py
+++ prob_simulacion.py
@@ -1,16 +1,35 @@
-def getProbability():
-     return 1/30
-    
+from student import student
+import random
 
-asking = False
-while (asking == False):
-    resp = input ('Desea conocer la probabilidad de escoger un estudiante de simulacion?(Y/N): ')
-    if (resp == 'y' or resp == 'Y'):
-        asking = True
-        print('Existe una probabilidad de: ', getProbability())
-    else:
-        if (resp == 'n' or resp == 'N'):
-            print('adios.')
-            asking = True
-    
+students = student
+student_selected = students
+count = None
+times = random.randrange(1,1000)
+list = []
 
+list.append(students('Mauricio', 0))
+list.append(students('Enrique', 0))
+list.append(students('Norlie', 0))
+list.append(students('Ashly', 0))
+list.append(students('Cesar', 0))
+list.append(students('Diego', 0))
+list.append(students('Estefano', 0))
+list.append(students('Jose Agreda', 0))
+list.append(students('Jose Diaz', 0))
+list.append(students('Maria', 0))
+list.append(students('Edermar', 0))
+list.append(students('Jose Angel', 0))
+list.append(students('Hector Salazar', 0))
+list.append(students('Jose Manuel', 0))
+
+
+for i in range(random.randrange(1,1000)):
+    x = random.randrange(len(list))
+    list[x].prob = list[x].prob + 1
+
+for obj in list:
+    if (count is None or obj.prob > count):
+        count = obj.prob
+        student_selected = obj
+
+print ('El estudiante escogido es: ', student_selected.name, 'con ', student_selected.prob, 'veces escogido en', times , 'simulaciones.')
